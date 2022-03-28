e267d1c3b79a48de757f0d44ae51b8b8e022bcf3
RealPitchie <pitchie@list.ru>
2022-03-28 08:51:35 +0300

    mobile UI fixes

--- Data/Models/Post.cs
+++ Data/Models/Post.cs
@@ -5,9 +5,10 @@ public class Post
     public string Id { get; set; }
     public string Title { get; set; }
     public string Subtitle { get; set; }
-    public string[] Text { get; set; }
+    public string Text { get; set; }
     public string Image { get; set; }
     public string Video { get; set; }
     public DateTime Posted { get; set; }
     public virtual ICollection<Comment> Comments { get; set; }
+    
 }
